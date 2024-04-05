9ac4cb5beb94a1ce77f3caca01ccb87a2e02f523
Conrad Miller <42632539+conradrmiller@users.noreply.github.com>
2024-04-05 17:44:47 -0400

    added links and updated colors, added memoji

--- src/components/About.js
+++ src/components/About.js
@@ -15,7 +15,7 @@ export default function About() {
                     <div className="flex justify-center">
                         <a
                             href="#contact"
-                            className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
+                            className="inline-flex text-white bg-lime border-0 py-2 px-6 focus:outline-none hover:bg-opacity-75 rounded text-lg"
                         >
                             Work With Me
                         </a>
@@ -31,7 +31,7 @@ export default function About() {
                     <img
                         className="object-cover object-center rounded"
                         alt="hero"
-                        src="./coding.svg"
+                        src="./memoji.png"
                     />
                 </div>
             </div>
